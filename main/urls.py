3eb24264652d4d5a079503ce57083eb025ef7943
ibkay998 <ibukun46@gmail.com>
2024-02-07 13:50:40 +0100

    j

--- main/urls.py
+++ main/urls.py
@@ -40,7 +40,7 @@ router = routers.DefaultRouter()
 # Schema view configuration
 schema_view = get_schema_view(
     openapi.Info(
-        title="SOLARAPP",
+        title="SOLARGEN",
         default_version='v1',
         description="Your API description",
         terms_of_service="https://www.example.com/policies/terms/",
@@ -48,7 +48,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    
+    permission_classes=(AllowAny,),
 )
 
 urlpatterns += [
@@ -60,6 +60,6 @@ urlpatterns += [
 urlpatterns += [
     path('', include(router.urls)),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
