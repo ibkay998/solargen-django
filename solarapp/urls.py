c79677a65589544bfaa15cfea33f0f677008fbb3
EMMANUEL OLUYOYIN O <46908791+Timmecom@users.noreply.github.com>
2024-02-15 11:58:57 +0100

    Added endpoint for installer to view profile

--- solarapp/urls.py
+++ solarapp/urls.py
@@ -3,14 +3,12 @@ from . import views
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-    # path('api/signup/', views.signup, name='signup'),
-    # path('api/login/', views.login, name='login'),
-    # path('api/users/', views.user_list, name='user-list'),
     path('installers/signup/', views.installer_signup, name='installer_signup'),
     path('installers/signin/', views.installer_signin, name='installer_signin'),
     path('installers/add_user/', views.add_user, name='add_user'),
     path('installers/change_password/', views.installer_change_password, name='installer_change_password'),
-    # path('installers/<int:installer_id>/users/<int:user_id>/user_initial_password/', views.get_user_initial_password, name='get_initial_password'),
+    path('installers/view_profile/', views.installer_view_profile, name='installer_view_profile'),
+
     path('users/signin/', views.user_signin, name='user_signin'),
     path('users/change_password/', views.user_change_password, name='user_change_password'),
 
