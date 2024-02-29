7a043ac5cc668e3180c84677a66f6b62c4bcb985
ibkay998 <ibukun46@gmail.com>
2024-02-29 01:45:57 +0100

    latest changes

--- solarapp/serializers.py
+++ solarapp/serializers.py
@@ -4,7 +4,7 @@ from .models import Installer,UserProfile
 from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import check_password,make_password
 from django.contrib.auth import get_user_model
-from .models import Installer, UserProfile, CustomUser
+from .models import Installer, UserProfile, CustomUser,SolarSpecification, InverterSpecification, BatteryStorageSpecification
 from rest_framework.validators import UniqueValidator
 from rest_framework import status
 from geopy.geocoders import Nominatim
@@ -27,6 +27,55 @@ def get_long_lat(country,state,city,street,houseNumber):
         if getLoc:
             return (getLoc.latitude, getLoc.longitude, getLoc.address)
     return (None, None, None)
+
+
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+    contact = serializers.CharField(required=True)
+    name_of_asset = serializers.CharField(required=True)
+    number_of_installed_assets = serializers.IntegerField(required=True)
+    latitude = serializers.FloatField(required=True)
+    longitude = serializers.FloatField(required=True)
+    solar_size_watts = serializers.IntegerField(required=True)
+    efficiency_ratings = serializers.IntegerField(required=True)
+    total_number_of_panels = serializers.IntegerField(required=True)
+    capacity_size = serializers.FloatField(required=True)
+    efficiency = serializers.FloatField(required=True)
+    voltage_input_range = serializers.FloatField(required=True)
+    total_capacity_kwh = serializers.FloatField(required=True)
+    voltage_v = serializers.FloatField(required=True)
+    conventional_pricing = serializers.FloatField(required=True)
+    ai_dynamic_pricing = serializers.FloatField(required=True)
+
+    class Meta:
+        model = UserProfile
+        fields = ['user_email', 'phone', 'name_of_asset', 'number_of_installed_assets',
+                  'latitude', 'longitude', 'solar_size_watts', 'efficiency_ratings',
+                  'total_number_of_panels', 'capacity_size', 'efficiency', 'voltage_input_range',
+                  'total_capacity_kwh', 'voltage_v', 'conventional_pricing', 'ai_dynamic_pricing']
+
+    def create(self, validated_data):
+        # Extract solar specification data
+        solar_spec_data = validated_data.pop('solar_specification', None)
+        # Extract inverter specification data
+        inverter_spec_data = validated_data.pop('inverter_specification', None)
+        # Extract battery storage specification data
+        battery_spec_data = validated_data.pop('battery_storage_specification', None)
+        
+        # Create UserProfile instance
+        user_profile = UserProfile.objects.create(**validated_data)
+        
+        # Create related models instances
+        if solar_spec_data:
+            SolarSpecification.objects.create(user_profile=user_profile, **solar_spec_data)
+        if inverter_spec_data:
+            InverterSpecification.objects.create(user_profile=user_profile, **inverter_spec_data)
+        if battery_spec_data:
+            BatteryStorageSpecification.objects.create(user_profile=user_profile, **battery_spec_data)
+        
+        return user_profile
 
 class InstallerSignUpSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
@@ -113,6 +162,8 @@ class InstallerSignInSerializer(serializers.Serializer):
         username = data.get('username')
         password = data.get('password')
 
+        
+
         if username and password:
             # Retrieve the user object using the provided username
             try:
@@ -132,25 +183,55 @@ class InstallerSignInSerializer(serializers.Serializer):
 
 
 class InstallerAddUserSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True,
-                                    #  validators=[UniqueValidator(queryset=CustomUser.objects.all())]
-                                     )
-    email = serializers.EmailField(required=True,
-                                #    validators=[UniqueValidator(queryset=CustomUser.objects.all())]
-                                   )
-    first_name = serializers.CharField(required=True,
-                                    #  validators=[UniqueValidator(queryset=CustomUser.objects.all())]
-                                     )
-    last_name = serializers.CharField(required=True,
-                                    #  validators=[UniqueValidator(queryset=CustomUser.objects.all())]
-                                     )
+    username = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    location_houseNumber = serializers.IntegerField(required=True)
+    location_street = serializers.CharField(required=True)
+    location_city = serializers.CharField(required=True)
+    location_state = serializers.CharField(required=True)
+    location_country = serializers.CharField(required=True)
+    contact = serializers.CharField(required=True)
+    solar_size_watts = serializers.IntegerField(required=True)
+    efficiency_ratings = serializers.IntegerField(required=True)
+    total_number_of_panels = serializers.IntegerField(required=True)
+    capacity_size = serializers.FloatField(required=True)
+    efficiency = serializers.FloatField(required=True)
+    voltage_input_range = serializers.FloatField(required=True)
+    total_capacity_kwh = serializers.FloatField(required=True)
+    voltage_v = serializers.FloatField(required=True)
+    conventional_pricing = serializers.FloatField(required=True)
+    ai_dynamic_pricing = serializers.FloatField(required=True)
 
     def validate(self, data):
         username = data.get('username')
         email = data.get('email')
         first_name = data.get('first_name')
         last_name = data.get('last_name')
+        country = data.get('location_country')
+        state = data.get('location_state')
+        city = data.get('location_city')
+        street = data.get('location_street')
+        houseNumber = data.get('location_houseNumber')
+        address_provided = f"{houseNumber}, {street}, {city}, {state}, {country}"
+        
+        other_names = serializers.CharField(required=False)
+        full_name = f"{first_name} {other_names} {last_name}"
+        
 
+        try:
+            latitude, longitude, address_found = get_long_lat(country, state, city, street, houseNumber)
+            data['latitude'] = latitude
+            data['longitude'] = longitude
+            data['address_found'] = address_found
+        except GeocoderTimedOut:
+            data['latitude'] = 0
+            data['longitude'] = 0
+            data['address_found'] = ''
+
+        data['address_provided'] = address_provided
+        data['full_name'] = full_name
 
         if username and email and first_name and last_name:
             if CustomUser.objects.filter(email=email).exists():
@@ -159,7 +240,65 @@ class InstallerAddUserSerializer(serializers.Serializer):
                 raise serializers.ValidationError(f'Username "{username}" already in use')
             return data
         else:
-            raise serializers.ValidationError('Username, Email, and Password are all required')
+            raise serializers.ValidationError('Username, Email, First Name, Last Name, and Contact are all required')
+
+    def create(self, validated_data):
+        # Extract solar specification data
+        solar_spec_data = {
+            'solar_size_watts': validated_data.pop('solar_size_watts'),
+            'efficiency_ratings': validated_data.pop('efficiency_ratings'),
+            'total_number_of_panels': validated_data.pop('total_number_of_panels')
+        }
+
+        # Extract inverter specification data
+        inverter_spec_data = {
+            'capacity_size': validated_data.pop('capacity_size'),
+            'efficiency': validated_data.pop('efficiency'),
+            'voltage_input_range': validated_data.pop('voltage_input_range')
+        }
+
+        # Extract battery storage specification data
+        battery_spec_data = {
+            'total_capacity_kwh': validated_data.pop('total_capacity_kwh'),
+            'voltage_v': validated_data.pop('voltage_v')
+        }
+
+        # Create CustomUser instance
+        
+        installer = self.context.get('installer')
+        hashed_password = make_password('password123')
+        validated_data['password'] = hashed_password
+        
+    
+        # Create CustomUser instance with the installer_profile
+        validated_data.pop('location_houseNumber', None)
+        validated_data.pop('location_street', None)
+        validated_data.pop('location_city', None)
+        validated_data.pop('location_state', None)
+        validated_data.pop('location_country', None)
+        validated_data["contact_number"] = validated_data["contact"]
+        validated_data.pop('contact', None)
+
+        
+
+        user_profile = UserProfile.objects.create(installer_profile=installer, **validated_data)
+
+        
+
+        # Create related models instances
+        SolarSpecification.objects.create(user_profile=user_profile, **solar_spec_data)
+        InverterSpecification.objects.create(user_profile=user_profile, **inverter_spec_data)
+        BatteryStorageSpecification.objects.create(user_profile=user_profile, **battery_spec_data)
+    
+
+        # Increment the installed_assets
+        installer.installed_assets += 1
+
+        # Save the installer
+        installer.save()
+        
+        
+        return user_profile
 
 class ChangePasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
--- solarapp/views.py
+++ solarapp/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.contrib.auth.models import User,auth
+from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
-from .serializers import InstallerProfileSerializer, InstallerProfileViewSerializer, UserProfileSerializer, UserProfileSignInSerializer, InstallerSignUpSerializer, InstallerAddUserSerializer, ChangePasswordSerializer
+from .serializers import (
+    InstallerProfileSerializer,
+    InstallerProfileViewSerializer,
+    UserProfileSerializer,
+    UserProfileSignInSerializer,
+    InstallerSignUpSerializer,
+    InstallerAddUserSerializer,
+    ChangePasswordSerializer,
+)
 from .models import Installer, UserProfile, CustomUser
 from rest_framework import viewsets
 from rest_framework.response import Response
 
 from rest_framework import status
-from rest_framework.decorators import api_view,permission_classes,authentication_classes
+from rest_framework.decorators import (
+    api_view,
+    permission_classes,
+    authentication_classes,
+)
 from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth.hashers import check_password,make_password
+from django.contrib.auth.hashers import check_password, make_password
 
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -37,107 +49,126 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = UserProfileSerializer
     queryset = UserProfile.objects.all()
 
-@swagger_auto_schema(method='post', request_body=InstallerSignUpSerializer)
-@api_view(['POST'])
+
+@swagger_auto_schema(method="post", request_body=InstallerSignUpSerializer)
+@api_view(["POST"])
 @authentication_classes([])
 @permission_classes([])
 def installer_signup(request):
-    if request.method == 'POST':
-        serializer_signup = InstallerSignUpSerializer(data = request.data)
+    if request.method == "POST":
+        serializer_signup = InstallerSignUpSerializer(data=request.data)
         if serializer_signup.is_valid():
             # Hash the password before saving
-            
-            hashed_password = make_password(serializer_signup.validated_data.get('password'))
-            latitude = serializer_signup.validated_data.get('latitude')
-            longitude = serializer_signup.validated_data.get('longitude')
-            address_found = serializer_signup.validated_data.get('address_found')
-            address_provided = serializer_signup.validated_data.get('address_provided')
-            company_name = serializer_signup.validated_data.get('company_name')
-            contact_number = serializer_signup.validated_data.get('contact_number')
-            full_name = serializer_signup.validated_data.get('full_name')
-            other_names = serializer_signup.validated_data.get('other_names')
 
-            
+            hashed_password = make_password(
+                serializer_signup.validated_data.get("password")
+            )
+            latitude = serializer_signup.validated_data.get("latitude")
+            longitude = serializer_signup.validated_data.get("longitude")
+            address_found = serializer_signup.validated_data.get("address_found")
+            address_provided = serializer_signup.validated_data.get("address_provided")
+            company_name = serializer_signup.validated_data.get("company_name")
+            contact_number = serializer_signup.validated_data.get("contact_number")
+            full_name = serializer_signup.validated_data.get("full_name")
+            other_names = serializer_signup.validated_data.get("other_names")
+
             serializer_profile = InstallerProfileSerializer(data=request.data)
             # Set the hashed password in the serializer_profile data
-            
+
             if serializer_profile.is_valid():
-                serializer_profile.validated_data['password'] = hashed_password
-                serializer_profile.validated_data.update({
-                'latitude': latitude,
-                'longitude': longitude,
-                'address_found': address_found,
-                'address_provided': address_provided,
-                'company_name': company_name,
-                'contact_number': contact_number,
-                'full_name': full_name,
-                'other_names': other_names,
-            })
+                serializer_profile.validated_data["password"] = hashed_password
+                serializer_profile.validated_data.update(
+                    {
+                        "latitude": latitude,
+                        "longitude": longitude,
+                        "address_found": address_found,
+                        "address_provided": address_provided,
+                        "company_name": company_name,
+                        "contact_number": contact_number,
+                        "full_name": full_name,
+                        "other_names": other_names,
+                    }
+                )
 
                 serializer_profile.save()
                 return Response(serializer_profile.data, status=status.HTTP_201_CREATED)
-            return Response(serializer_profile.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                serializer_profile.errors, status=status.HTTP_400_BAD_REQUEST
+            )
         return Response(serializer_signup.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@swagger_auto_schema(method='post', request_body=InstallerSignInSerializer)
-@api_view(['POST'])
+
+@swagger_auto_schema(method="post", request_body=InstallerSignInSerializer)
+@api_view(["POST"])
 @authentication_classes([])
 @permission_classes([])
 def installer_signin(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         serializer_signin = InstallerSignInSerializer(data=request.data)
         if serializer_signin.is_valid():
-            installer = serializer_signin.validated_data['installer']
+            installer = serializer_signin.validated_data["installer"]
             # Authentication successful, generate JWT token
             refresh = RefreshToken.for_user(installer)
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-            }, status=status.HTTP_200_OK)
+            return Response(
+                {
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                },
+                status=status.HTTP_200_OK,
+            )
         else:
-            return Response(serializer_signin.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                serializer_signin.errors, status=status.HTTP_400_BAD_REQUEST
+            )
 
-@swagger_auto_schema(method='post', request_body=UserProfileSerializer, 
-                     manual_parameters=[
-    openapi.Parameter('Authorization', openapi.IN_HEADER, description="Description of custom header", type=openapi.TYPE_STRING)]
-    )
-@api_view(['POST'])
+
+@swagger_auto_schema(
+    method="post",
+    request_body=InstallerAddUserSerializer,
+    manual_parameters=[
+        openapi.Parameter(
+            "Authorization",
+            openapi.IN_HEADER,
+            description="Description of custom header",
+            type=openapi.TYPE_STRING,
+        )
+    ],
+)
+@api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def add_user(request):
-    if request.method == 'POST':
-        serializer_addUser = InstallerAddUserSerializer(data=request.data)
-        if serializer_addUser.is_valid():
-            serializer_profile = UserProfileSerializer(data=request.data)
-            if serializer_profile.is_valid():# and serializer_profile2.is_valid():
-                installer = request.user.installer
-                custom_header_value = request.headers.get('Authorization')
+    if request.method == "POST":
+        installer = request.user
+        installer_instance = Installer.objects.get(username=installer.username)
+        # Pass the installer to the serializer during initialization
+        serializer_add_user = InstallerAddUserSerializer(
+            data=request.data, context={"installer": installer_instance}
+        )
+        if serializer_add_user.is_valid():
+            # Save the user profile using the serializer's save method
+            serializer_add_user.save()
 
-                # Hash the default password
-                hashed_password = make_password('password123')
-                serializer_profile.validated_data['password'] = hashed_password
+            return Response("User Successfully created", status=status.HTTP_201_CREATED)
 
-                # Set the installer
-                serializer_profile.validated_data['installer_profile'] = installer
-                
-                installer.installed_assets += 1
-                installer.save()
-                
-                # Save the user profile
-                serializer_profile.save()
-
-                return Response(serializer_profile.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer_addUser.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer_add_user.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@swagger_auto_schema(method='post', request_body=ChangePasswordSerializer,
-                     manual_parameters=[
-    openapi.Parameter('Authorization', openapi.IN_HEADER, description="Description of custom header", type=openapi.TYPE_STRING)]
-    )
-@api_view(['POST'])
+@swagger_auto_schema(
+    method="post",
+    request_body=ChangePasswordSerializer,
+    manual_parameters=[
+        openapi.Parameter(
+            "Authorization",
+            openapi.IN_HEADER,
+            description="Description of custom header",
+            type=openapi.TYPE_STRING,
+        )
+    ],
+)
+@api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def installer_change_password(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         serializer = ChangePasswordSerializer(data=request.data)
         if serializer.is_valid():
             user = request.user
@@ -145,31 +176,48 @@ def installer_change_password(request):
             try:
                 user_profile = Installer.objects.get(username=user.username)
             except Installer.DoesNotExist:
-                return Response({'error': 'Installer not found'}, status=status.HTTP_404_NOT_FOUND)
+                return Response(
+                    {"error": "Installer not found"}, status=status.HTTP_404_NOT_FOUND
+                )
 
             # print(serializer.validated_data['new_password'], user_profile.password)
             # Check if the old password matches the current password of the user
-            if not check_password(serializer.validated_data['old_password'], user_profile.password):
-                return Response({'error': 'Invalid old password'}, status=status.HTTP_400_BAD_REQUEST)
+            if not check_password(
+                serializer.validated_data["old_password"], user_profile.password
+            ):
+                return Response(
+                    {"error": "Invalid old password"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
 
             # Update the user's password
-            user_profile.set_password(serializer.validated_data['new_password'])
+            user_profile.set_password(serializer.validated_data["new_password"])
             user_profile.save()
 
-            return Response({'message': 'Password updated successfully'}, status=status.HTTP_200_OK)
+            return Response(
+                {"message": "Password updated successfully"}, status=status.HTTP_200_OK
+            )
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@swagger_auto_schema(method='get', manual_parameters=[
-    openapi.Parameter('Authorization', openapi.IN_HEADER, description="Description of custom header", type=openapi.TYPE_STRING)]
-    )
-@api_view(['GET'])
+@swagger_auto_schema(
+    method="get",
+    manual_parameters=[
+        openapi.Parameter(
+            "Authorization",
+            openapi.IN_HEADER,
+            description="Description of custom header",
+            type=openapi.TYPE_STRING,
+        )
+    ],
+)
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def installer_view_profile(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         serializer = InstallerProfileViewSerializer(data=request.data)
-        if serializer.is_valid():    
+        if serializer.is_valid():
             installer_username = request.user.username
             installer = Installer.objects.get(username=installer_username)
 
@@ -185,39 +233,50 @@ def installer_view_profile(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 """
 USERS
 """
 
 
-@swagger_auto_schema(method='post', request_body=UserProfileSignInSerializer)
-@api_view(['POST'])
+@swagger_auto_schema(method="post", request_body=UserProfileSignInSerializer)
+@api_view(["POST"])
 @authentication_classes([])
 @permission_classes([])
 def user_signin(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         serializer = UserProfileSignInSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.validated_data['user']
+            user = serializer.validated_data["user"]
 
             # Authentication successful, generate JWT token
             refresh = RefreshToken.for_user(user)
-            return Response({
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-            }, status=status.HTTP_200_OK)
+            return Response(
+                {
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                },
+                status=status.HTTP_200_OK,
+            )
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@swagger_auto_schema(method='post', request_body=ChangePasswordSerializer,
-                     manual_parameters=[
-    openapi.Parameter('Authorization', openapi.IN_HEADER, description="Description of custom header", type=openapi.TYPE_STRING)]
-    )
-@api_view(['POST'])
+
+@swagger_auto_schema(
+    method="post",
+    request_body=ChangePasswordSerializer,
+    manual_parameters=[
+        openapi.Parameter(
+            "Authorization",
+            openapi.IN_HEADER,
+            description="Description of custom header",
+            type=openapi.TYPE_STRING,
+        )
+    ],
+)
+@api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def user_change_password(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         serializer = ChangePasswordSerializer(data=request.data)
         if serializer.is_valid():
             user = request.user
@@ -225,17 +284,27 @@ def user_change_password(request):
             try:
                 user_profile = UserProfile.objects.get(username=user.username)
             except UserProfile.DoesNotExist:
-                return Response({'error': 'User profile not found'}, status=status.HTTP_404_NOT_FOUND)
+                return Response(
+                    {"error": "User profile not found"},
+                    status=status.HTTP_404_NOT_FOUND,
+                )
 
             # print(serializer.validated_data['new_password'], user_profile.password)
             # Check if the old password matches the current password of the user
-            if not check_password(serializer.validated_data['old_password'], user_profile.password):
-                return Response({'error': 'Invalid old password'}, status=status.HTTP_400_BAD_REQUEST)
+            if not check_password(
+                serializer.validated_data["old_password"], user_profile.password
+            ):
+                return Response(
+                    {"error": "Invalid old password"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
 
             # Update the user's password
-            user_profile.set_password(serializer.validated_data['new_password'])
+            user_profile.set_password(serializer.validated_data["new_password"])
             user_profile.save()
 
-            return Response({'message': 'Password updated successfully'}, status=status.HTTP_200_OK)
+            return Response(
+                {"message": "Password updated successfully"}, status=status.HTTP_200_OK
+            )
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
